0dddc0d85f73d036d4dc8bcdad002c590bd77f0d
Vasyl Poidynets <Vasyl_Poidynets@epam.com>
2021-02-17 12:55:47 +0200

    added LWC and input fields

--- force-app/main/default/lwc/createRequestRecord/createRequestRecord.js
+++ force-app/main/default/lwc/createRequestRecord/createRequestRecord.js
@@ -1,2 +1,18 @@
 import { LightningElement } from 'lwc';
-export default class CreateRequestRecord extends LightningElement {}
+import LICENSE_ORDER_OBJECT from '@salesforce/schema/License_Order__c';
+import APPLICATION_NAME_FIELD from '@salesforce/schema/License_Order__c.Application_Name__c';
+import ACCOUNT_NAME_FIELD from '@salesforce/schema/License_Order__c.Account_Name__c';
+import CONTACT_NAME_FIELD from '@salesforce/schema/License_Order__c.Contact_Name__c';
+import NOTES_FIELD from '@salesforce/schema/License_Order__c.Notes__c';
+
+export default class CreateRequestRecord extends LightningElement {
+    licenseOrderObject = LICENSE_ORDER_OBJECT;
+    applicationNameField = APPLICATION_NAME_FIELD;
+    accountNameField = ACCOUNT_NAME_FIELD;
+    contactNameField = CONTACT_NAME_FIELD;
+    notesField = NOTES_FIELD;
+
+    handleAccountCreated(){
+        // Run code when account is created.
+    }
+}
